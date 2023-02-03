270996789b11b318b5c142c8e95d2b10e0849401
CBrandsDev <cbrandao2001@gmail.com>
2023-02-03 15:42:11 -0300

    finished

--- script/index.js
+++ script/index.js
@@ -1,24 +1,38 @@
-var button = document.querySelectorAll(".rate");
-var result=document.getElementById(".selres");
-var submit=document.querySelector(".submit");
-var card=document.querySelector(".card");
-var thanks=document.querySelector(".thanks")
-var please=document.querySelector(".plsrateus");
+'use strict';
 
+const container = document.querySelector('.container');
+const thanksContainer = document.querySelector('.thanks');
 
+const submitButton = document.getElementById('sb');
 
-submit.addEventListener("click", changePage);
+const selRes = document.getElementById('selres');
+const rates = document.querySelectorAll('.rates');
 
-function changePage(){
-    if(result.innerHTML==''){
-    please.classList.remove("hidden");
-    }
-    else{
-        card.style.display="none";
-        thanks.classList.remove("hidden");
-    }
-}
+thanksContainer.addEventListener('click', function () {
+    container.style.display = 'block';
+    thanksContainer.classList.add('hidden');
+    selRes.innerHTML = '';
+    document.querySelector('.result').textContent = 'Please Select Number'
+    //Short fix problem for now.
+})
 
+rates.forEach(rate => {
+    rate.addEventListener('click', () => {
+        selRes.innerHTML = (rate.innerHTML)
+    })
+    console.log(rate.innerHTML, rate);
+})
+//If no number clicked It wont submit. But second time when rate it submit somehow, I will be pleased if you give feedback for solve this. For now  when second rate no number selected, it writes 'please select number'. 
+rates.forEach(occurence => {
+    let id = occurence.getAttribute('id');
+    occurence.addEventListener('click', function () {
+
+        submitButton.addEventListener('click', () => {
+            thanksContainer.classList.remove('hidden');
+            container.style.display = 'none'
+        })
+    });
+});
 
 
 button.forEach(function(x){
